d137606fc496cc049637b3945a6204c154c83664
Marshal Mori Cavalheiro <marshal@fox.debian>
2019-12-05 13:20:40 -0300

    Subindo para o server

--- aluno/form.py
+++ aluno/form.py
@@ -5,7 +5,7 @@ from wtforms.fields.html5 import EmailField
 class RegisterForm(FlaskForm):
     fullname = StringField('Nome Completo', [validators.Required()])
     email = EmailField('Email', [validators.Required()])
-    username = StringField('Nome de usuário', [validators.Required(), validators.Length(min=4, max=25)])
+    username = StringField('Nome de usuario', [validators.Required(), validators.Length(min=4, max=25)])
     password = PasswordField('Senha', [validators.Required(), 
                                        validators.EqualTo('confirm', message='As senhas devem ser iguais.'), 
                                        validators.Length(min=4, max=80)])
--- aluno/views.py
+++ aluno/views.py
@@ -2,10 +2,6 @@ from yoda import app
 from flask import render_template, redirect, url_for
 from aluno.form import RegisterForm
 
-# @app.route('/login')
-# def login():
-#     return 'Olá Aluno'
-
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
